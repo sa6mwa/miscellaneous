cbd803427913d3e09d9c6737311d01c3f4a176e7
Michel <sa6mwa@radiohorisont.se>
2020-05-10 20:14:38 +0200

    RF to DC rectifier code improvements

--- scripts/rectified-dc-voltage-to-watts.py
+++ scripts/rectified-dc-voltage-to-watts.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 import sys
+import math
 
+fvd = 0.2 # forward voltage drop (across the diode)
 fpp = 2 # floating point precision
-rms = 0.70710678 # rms multiplier = 1/sqrt(2)
+#rms = 0.70710678 # rms multiplier = 1/sqrt(2)
+rms = 1.0/math.sqrt(2)
 dcv_loaded = 0.0
 dcv_unloaded = 0.0
 rms_loaded = 0.0
@@ -55,14 +58,14 @@ if dcv_unloaded > 0.0 and dcv_unloaded < 0.5:
 if dcv_loaded < 0.5:
   usage()
 
-
+print "Forward voltage drop (V) = {fvd}".format(fvd=round(fvd, fpp))
 print "Loaded peak voltage (V) = {dcv_loaded}".format(dcv_loaded=round(dcv_loaded, fpp))
-rms_loaded = dcv_loaded * rms
+rms_loaded = (dcv_loaded + fvd) * rms
 print "Loaded RMS voltage (V) = {rms_loaded}".format(rms_loaded=round(rms_loaded, fpp))
 
 if dcv_unloaded > 0:
   print "Unloaded peak voltage (V) = {dcv_unloaded}".format(dcv_unloaded=round(dcv_unloaded, fpp))
-  rms_unloaded = dcv_unloaded * rms
+  rms_unloaded = (dcv_unloaded + fvd) * rms
   print "Unloaded RMS voltage (V) = {rms_unloaded}".format(rms_unloaded=round(rms_unloaded, fpp))
   # calculate impedance
   z = (r * (rms_unloaded - rms_loaded)) / rms_loaded
@@ -75,3 +78,17 @@ print "Impedance (Z in Ohm) = {z}".format(z=round(z, fpp))
 pwr = (rms_loaded ** 2.0) / z
 
 print "Power (Watts) = {pwr}".format(pwr=round(pwr, fpp))
+
+
+w5v = round(math.sqrt(5.0 * z) / rms - fvd, fpp)
+w3v = round(math.sqrt(3.0 * z) / rms - fvd, fpp)
+w1v = round(math.sqrt(1.0 * z) / rms - fvd, fpp)
+w0v5 = round(math.sqrt(0.5 * z) / rms - fvd, fpp)
+
+print """
+CHEAT-SHEET PEAK VOLTAGE FOR PREDEFINED POWER LEVELS
+  5W peak voltage = {w5v}
+  3W peak voltage = {w3v}
+  1W peak voltage = {w1v}
+  0.5W peak voltage = {w0v5}
+""".format(w5v=w5v, w3v=w3v, w1v=w1v, w0v5=w0v5)
